0e7c6747dc80bde147a5fc372f9b8ad4ba9f734c
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-29 13:27:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   return_string = ""
   if array.length == 1
-   return array[0]
-    elsif array.length == 2 
-     array.join(" and ") 
-   else array.each do |word|
+    return array[0]
+  elsif array.length == 2 
+     return array.join(" and ") 
+  else array.each do |word|
      end
   end
   return_string
